b73c3259a59af9b36e34829fd60cb2ee062134d2
Jørgen Tellnes <jorgen@tellnes.nu>
2016-05-30 19:13:49 +0200

    Add model and migration for Song

---
+++ app/Song.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'musicBy', 'lyricsBy'
+    ];
+}
---
+++ database/migrations/2016_05_30_165128_create_songs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('title');
+            $table->text('musicBy');
+            $table->text('lyricsBy');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('songs');
+    }
+}
