6482ccdaf334380dec4d1d472cd238a964c22fd3
Benjamin Dehli <superelg@gmail.com>
2016-05-30 23:21:17 +0200

    Add example welcome screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,79 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Laravel</title>
+<head>
+    <title>FlowGig</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link href="/css/app.css" rel="stylesheet" type="text/css">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <style>
+        html, body {
+            height: 100%;
+            background: url("/images/jpg/background.jpg");
+            background-size: cover;
+        }
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+        }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+            background-color: rgba(0, 0, 0, .3);
+        }
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+        .content {
+            text-align: left;
+            display: inline-block;
+        }
 
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+        .input-group {
+            text-align: left;
+        }
+
+        .title {
+            font-size: 96px;
+            color: #FFF;
+            text-shadow: 0 0 20px rgba(0, 0, 0, .8);
+        }
+
+        .login-box {
+            min-width: 350px;
+            margin-top: 10px;
+        }
+
+        button[disabled]:hover {
+            cursor: not-allowed;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">FlowGig</div>
+        <div class="box z-2">
+            <div class="content login-box">
+                <div class="input-group">
+                    <input id="username" type="text"/><label for="username">Username</label>
+                </div>
+                <div class="input-group">
+                    <input id="password" type="password"/><label for="password">Password</label>
+                </div>
+                <div class="input-group float-right">
+                    <button class="button button-flat" disabled="disabled">Register</button>
+                    <button class="button button-primary button-raised">Log in</button>
+                    <div class="clearfix"></div>
+                </div>
             </div>
         </div>
-    </body>
+    </div>
+</div>
+<script src="/js/all.js" type="application/javascript"></script>
+
+</body>
 </html>
