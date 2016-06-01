05a34e3777b6a0e0ceebb53397247048816baf31
Jørgen Tellnes <jorgen@tellnes.nu>
2016-06-02 00:04:30 +0200

    Add database seeder for Setlist

---
+++ database/seeds/SetlistsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Setlist;
+use Illuminate\Database\Seeder;
+
+class SetlistsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('setlists')->truncate();
+
+        Setlist::create(['title' => 'Christmas Gig']);
+
+        Setlist::create(['title' => 'Studio, Best of album']);
+
+        Setlist::create(['title' => 'Rehearsal, New Years Gig']);
+    }
+}
