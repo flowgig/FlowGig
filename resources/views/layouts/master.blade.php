4aab6649a4871406a37f547eb5a5201877a5aabe
Benjamin Dehli <superelg@gmail.com>
2016-06-05 18:11:09 +0200

    Add background image to master view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,13 +2,24 @@
 <head>
     <title>FlowGig - @yield('title')</title>
     <link rel="stylesheet" href="/css/app.css" type="text/css"/>
-
+    <style>
+        html, body {
+            height: 100%;
+            background: url("/images/jpg/background.jpg");
+            background-attachment: fixed;
+            background-size: cover;
+        }
+        .main-content {
+            min-height: 100%;
+            background-color: rgba(0, 0, 0, .5);
+        }
+    </style>
 </head>
 <body>
 <nav class="navbar navbar-dark z-2">
     <a class="sidenav-toggle hide-big"><i class="sidenav-toggle-logo fa fa-bars"></i></a>
     <a class="logo">
-        <img src="/images/svg/flowgig-logo-white.svg" alt="FlowGig logo" />
+        <img src="/images/svg/flowgig-logo-white.svg" alt="FlowGig logo"/>
     </a>
     <span class="menu-divider hide-xsmall"></span>
     <span class="hide-big hide-xsmall"></span>
@@ -25,7 +36,7 @@
     <div>
         <div class="sidenav-logo">
             <a class="sidenav-toggle">
-                <img src="/images/svg/flowgig-logo-black.svg" alt="FlowGig logo" />
+                <img src="/images/svg/flowgig-logo-black.svg" alt="FlowGig logo"/>
                 <i class="fa fa-angle-left float-right"></i>
             </a>
         </div>
