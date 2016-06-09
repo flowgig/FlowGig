cf1162869553d01109df3bcc4212b5e707dd7257
Benjamin Dehli <superelg@gmail.com>
2016-06-09 22:01:40 +0200

    Add actionbutton and modal for add song

--- resources/views/setlist/edit.blade.php
+++ resources/views/setlist/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('title', 'Setlist - ' . $setlist->title)
 @section('content')
-    <div id="setlist">
+    <div id="setlist" xmlns:v-on="http://www.w3.org/1999/xhtml">
         <div class="content">
             <div class="box z-1">
                 <div class="content">
@@ -13,21 +13,38 @@
                 </div>
             </div>
         </div>
-        <div id="repertoire" class="actionbar-modal">
+        <div id="repertoire" class="actionbar-modal repertoire-modal">
             @include('setlist._repertoire', $repertoire)
+        </div>
+        <div id="repertoire" class="actionbar-modal add-song-modal">
+            <div class="modal-header">Repertoire:</div>
+            <div class="modal-content">
+                <div class="input-group">
+                    <input id="title" type="text"/><label for="title">Title</label>
+                </div>
+                <div class="input-group">
+                    <input id="lyrics_by" type="text"/><label for="lyrics_by">Lyrics by</label>
+                </div>
+                <div class="input-group">
+                    <input id="music_by" type="text"/><label for="music_by">Music by</label>
+                </div>
+                <button v-on:click="addSong(song)">add</button>
+
+            </div>
+
         </div>
         <div class="action-button">
             <i class="icon fa fa-plus"></i>
         </div>
         <div class="action-menu">
-            <button class="toggle-action-button toggle-elements tooltip" id="" title="Add song from repertoire" value="actionbar-modal">
+            <button class="toggle-action-button toggle-elements tooltip" data-type="radio" title="Add song from repertoire" value="repertoire-modal">
             <span class="icon fa fa-list">
             </span>
             </button>
-            <!--<button class="toggle-action-button toggle-elements tooltip" id="" title="Add new song" value="">
+            <button class="toggle-action-button toggle-elements tooltip" data-type="radio" title="Add new song" value="add-song-modal">
                 <span class="icon fa fa-plus">
                 </span>
-            </button> -->
+            </button>
         </div>
     </div>
     <script>
