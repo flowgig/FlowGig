d7de3603f17eb363957261c696487604ea813c53
Benjamin Dehli <superelg@gmail.com>
2016-07-07 20:33:48 +0200

    Adjust GUI for create-views

--- resources/views/song/create.blade.php
+++ resources/views/song/create.blade.php
@@ -5,8 +5,21 @@
     <div class="content">
         <div class="box">
             <div class="content">
+                <ol itemscope itemtype="http://schema.org/BreadcrumbList" class="breadcrumbs">
+                    <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+                        <a itemprop="item" href="{{ route('song.index') }}">
+                            <span itemprop="name">Songs</span>
+                        </a>
+                        <meta itemprop="position" content="1"/>
+                    </li>
+                    <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+                        <a itemprop="item" href="{{ route('song.create') }}">
+                            <span itemprop="name">Create new song</span>
+                        </a>
+                        <meta itemprop="position" content="2"/>
+                    </li>
+                </ol>
                 <h1>Create new song</h1>
-                <p>&nbsp;</p>
                 <form action="{{ route('song.store') }}" method="POST">
                     {{ csrf_field() }}
                     <div class="row">
@@ -14,16 +27,13 @@
                             <input type="text" name="title" id="title" placeholder="The song title"/>
                             <label for="title">Title</label>
                         </div>
-                    </div>
-                    <div class="clearfix"></div>
-                    <div class="row">
+
+
                         <div class="input-group col-sm-4">
                             <input type="text" name="music_by" id="music-by" placeholder="The song composer"/>
                             <label for="music-by">Music by</label>
                         </div>
-                    </div>
-                    <div class="clearfix"></div>
-                    <div class="row">
+
                         <div class="input-group col-sm-4">
                             <input type="text" name="lyrics_by" id="lyrics-by" placeholder="The lyrics author"/>
                             <label for="lyrics-by">Lyrics by</label>
