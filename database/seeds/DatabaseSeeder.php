c306a5cf780d5934a8b951a4601215e2663bdeab
Jørgen Tellnes <jorgen@tellnes.nu>
2016-07-29 17:11:03 +0200

    Add users table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UsersTableSeeder::class);
         $this->call(BandsTableSeeder::class);
         $this->call(SongsTableSeeder::class);
         $this->call(SetlistsTableSeeder::class);
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        User::create([
+            'name' => 'Bob Rocker',
+            'email' => 'bob@flowgig.com',
+            'password' => bcrypt('fender'),
+        ]);
+    }
+}
