6c5f9355eda90b5932d990d99b32e33a0ea6ebd9
Benjamin Dehli <superelg@gmail.com>
2016-07-31 17:33:11 +0200

    Add band boxes to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,14 +14,33 @@
                     </li>
                 </ol>
                 <h1>My FlowGig</h1>
+                <p>Name: {{Auth::user()->name}}</p>
+                {{Auth::user()->email}}
                 <h2>Bands</h2>
                 <div class="content">
-                   @foreach($bands as $band)
-                        <div class="button border-row float-left">
-                            <a href="{{ route('band.show', $band) }}">{{ $band->name }}</a>
-                        </div>
-                    @endforeach
-                    <div class="clearfix"></div>
+                    <div class="row">
+                        @foreach($bands as $band)
+                            <div class='col-xs-12 col-sm-6 col-md-4 col-lg-3'>
+                                <div class="box primary z-1">
+                                    <div class="box-header">
+                                        <div class="content">
+                                            <h3 class="box-title">{{ $band->name }}</h3>
+                                        </div>
+                                    </div>
+
+                                    <div class='content'>
+                                        <div class="button button-flat float-right">
+                                            <a href="{{ route('band.show', $band) }}">Show band</a>
+                                        </div>
+                                        <div class="clearfix"></div>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        @endforeach
+                        <div class="clearfix"></div>
+                    </div>
                 </div>
             </div>
         </div>
