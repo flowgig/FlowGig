8ecfa36a326a71c686194425ba434413cd36651e
Benjamin Dehli <superelg@gmail.com>
2016-07-31 15:49:48 +0200

    Add partial views for navigation bar

---
+++ resources/views/layouts/navbar-band.blade.php
@@ -0,0 +1,66 @@
+<nav class="navbar navbar-dark z-2">
+    <a class="sidenav-toggle hide-big"><i class="sidenav-toggle-logo fa fa-bars"></i></a>
+    <a class="logo" href="/">
+        <img src="/images/svg/flowgig-logo-white.svg" alt="FlowGig logo">
+    </a>
+    <span class="menu-divider hide-xsmall"></span>
+    <span class="hide-big hide-xsmall navbar-page-title"> @yield('navbar-title')</span>
+    <div class="main-menu hide-medium hide-small hide-xsmall">
+        <div class="menu-link">
+            <ul class="">
+                <li><a href="/song"><span class="icon fa fa-music"></span> Songs</a></li>
+                <li><a href="/setlist"><span class="icon fa fa-list-ol"></span> Setlists</a></li>
+                <li><a href="/dashboard"></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="main-menu-user float-right hide-medium hide-small hide-xsmall">
+        <ul>
+            <li class="dropdown-menu">
+                <span class="fa fa-user"></span>
+                <span class="hide-medium hide-small hide-xsmall"> {{Auth::user()->name}}</span>
+                <div class="dropdown-content">
+                    <div class="dropdown-profile-content dropdown-content-container">
+                        <span class="dropdown-profile-name">{{Auth::user()->name}}</span>
+                        <span class="dropdown-profile-email">{{Auth::user()->email}}</span>
+                        <div class="button-row">
+                            <a class="button button-flat button-default" href="/dashboard">Dashboard</a>
+                            <a class="button button-flat button-default" href="/dashboard">My account</a>
+                        </div>
+                    </div>
+                    <span class="separator"></span>
+                    {{-- <span class="dropdown-title">My bands</span>
+                     <ul class="list hover-list">
+                         @foreach($bands as $band)
+                             <li><a href="{{ route('band.show', $band) }}">{{ $band->name }}</a></li>
+                         @endforeach
+                     </ul>--}}
+                    <div class="button-row dropdown-content-container">
+                        <a class="button button-flat button-default float-right" href="/dashboard">Sign out</a>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+<div class="left-menu no-padding">
+    <div>
+        <div class="sidenav-logo">
+            <a class="sidenav-toggle">
+                <img src="/images/svg/flowgig-logo-black.svg" alt="FlowGig logo">
+                <i class="fa fa-angle-left float-right"></i>
+                <span class="clearfix"></span>
+            </a>
+        </div>
+        <ul class="">
+            <li><a href="/song"><span class="icon fa fa-music"></span> Songs</a></li>
+            <li><a href="/setlist"><span class="icon fa fa-list-ol"></span> Setlists</a></li>
+        </ul>
+        <ul class="">
+            <li><a href="/dashboard"><span class="icon fa fa-dashboard"></span> Dashboard</a></li>
+            <li><a href="#"><span class="icon fa fa-user"></span> My account</a></li>
+            <li><a href="#"><span class="icon fa fa-sign-out"></span> Sign out</a></li>
+        </ul>
+    </div>
+</div>
