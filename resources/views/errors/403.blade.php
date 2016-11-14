47723ffb83be3faca63ba4cc8bca7cd13ee32c0c
Jørgen Tellnes <jorgen@tellnes.nu>
2016-11-14 21:43:50 +0100

    Add unauthorized-view

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+@section('title', 'Unauthorized')
+@section('navbar-title', 'Unauthorized')
+@section('content')
+    <div class="content">
+        <div class="box">
+            <div class="content">
+                <h1>Unauthorized</h1>
+            </div>
+        </div>
+    </div>
+@endsection
