d3dcd6101f1fd7f53e4403770a7253a475111ab1
benjamindehli <superelg@gmail.com>
2017-02-05 01:05:48 +0100

    Use Laravel default paths for Laravel Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,8 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/app.js', 'dist/')
-   .sass('src/app.scss', 'dist/');
+mix.js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css');
 
 // Full API
 // mix.js(src, output);
