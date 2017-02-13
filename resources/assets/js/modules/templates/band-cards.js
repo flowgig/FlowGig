1af35903f1b5223de153c707053cb110f68b3e77
benjamindehli <superelg@gmail.com>
2017-02-13 23:35:41 +0100

    Add functions for band cards module

--- resources/assets/js/modules/templates/band-cards.js
+++ resources/assets/js/modules/templates/band-cards.js
@@ -1,35 +1,71 @@
 var quark = require('quark-gui');
+var axios = require('axios');
+var grid = quark.molecules.sections.grid;
 var card = quark.organisms.cards.card;
 var listMenu = quark.organisms.menus.listMenu;
-console.log(listMenu);
 
+function createListItems(band){
+    var listItems = [
+        {
+            title: 'Songs'
+        },
+        {
+            title: 'Gigs'
+        },
+        {
+            title: 'Members'
+        }
+    ];
+    return listItems;
+}
 
-module.exports =
-    card({
-        id: 'card2',
-        title: 'Primary card',
+function createCardElement(band) {
+    var cardElement = card({
+        id: 'band-card-' + band.id,
+        title: band.name,
         theme: 'primary',
         content: listMenu({
-            id: 'card-list-menu2',
+            id: 'band-card-list-' + band.id,
             hover: true,
-            listItems: [
-                {
-                    title: 'List item with subtitle',
-                    subTitle: 'This is a subtitle',
-                },
-                {
-                    title: 'List item without subtitle',
-                },
-                {
-                    title: 'List item with link and subtitle',
-                    subTitle: 'This is a subtitle',
-                    link: '#'
-                },
-                {
-                    title: 'List item with link',
-                    link: '#'
-                },
-            ]
+            listItems: createListItems(band)
         })
+    });
+    return cardElement;
+}
 
+function createGridElement(bands) {
+    var gridItems = [];
+    bands.forEach(function(band){
+        gridItems.push({
+            sizes: {
+                xs: 12,
+                sm: 6,
+                md: 4,
+                lg: 4
+            },
+            content: createCardElement(band)
+        });
+    });
+    var gridElement = grid({
+        gridItems: gridItems
     })
+    return gridElement;
+}
+
+function getCards(containerId) {
+    var containerElement = document.getElementById(containerId) !== null ? document.getElementById(containerId) : false;
+
+    axios.get('/quark').then(
+        function (response) {
+            var bands = response.data;
+            var gridElement = createGridElement(bands);
+
+            containerElement.innerHTML = gridElement;
+        }
+    )
+}
+
+export default function (containerId) {
+    return getCards(containerId);
+}
+
