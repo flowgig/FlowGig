f6510ab56cb362b8e15e528ede1334c91edbafeb
benjamindehli <superelg@gmail.com>
2017-02-16 23:25:51 +0100

    Update Laravel Mix And Vue.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,15 +11,16 @@ let mix = require('laravel-mix');
  |
  */
 
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+//var ExtractTextPlugin = require('extract-text-webpack-plugin');
+/*
 
 const sassLoader = ExtractTextPlugin.extract({
     loader: 'css-loader!sass-loader'
 });
-
-mix.webpackConfig({
-    module: {
+*/
+//mix.webpackConfig({
+  /*  module: {
         rules: [
             {
                 test: /\.scss$/,
@@ -27,30 +28,36 @@ mix.webpackConfig({
                 exclude: '/node_modules/'
             }
         ]
-    },
-    plugins: [
+    },*/
+ /*   plugins: [
         new ExtractTextPlugin('css/app.css')
     ]
-});
+});*/
 
 mix.js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css')
 
 // Full API
 // mix.js(src, output);
+// mix.react(src, output); <-- Identical to mix.js(), but registers React Babel compilation.
 // mix.extract(vendorLibs);
 // mix.sass(src, output);
 // mix.less(src, output);
+// mix.stylus(src, output);
 // mix.browserSync('my-site.dev');
 // mix.combine(files, destination);
+// mix.babel(files, destination); <-- Identical to mix.combine(), but also includes Babel compilation.
 // mix.copy(from, to);
 // mix.minify(file);
 // mix.sourceMaps(); // Enable sourcemaps
 // mix.version(); // Enable versioning.
 // mix.disableNotifications();
 // mix.setPublicPath('path/to/public');
+// mix.setResourceRoot('prefix/for/resource/locators');
 // mix.autoload({}); <-- Will be passed to Webpack's ProvidePlugin.
 // mix.webpackConfig({}); <-- Override webpack.config.js, without editing the file directly.
 // mix.then(function () {}) <-- Will be triggered each time Webpack finishes building.
 // mix.options({
-//   extractVueStyles: false // default
+//   extractVueStyles: false, // Extract .vue component styling to file, rather than inline.
+//   processCssUrls: true // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
 // });
