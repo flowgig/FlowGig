97a950ff7f823618fc25254219e9ee4b448e6e62
Jørgen Tellnes <jorgen@tellnes.nu>
2017-02-21 21:54:21 +0100

    Update songs table seeding

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -23,12 +23,27 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 });
 
 $factory->define(App\Song::class, function (Faker\Generator $faker) {
+
+    $musicalKeys =
+        [
+            'C', 'D', 'E', 'F', 'G', 'A', 'H',
+            'Cm', 'Dm', 'Em', 'Fm', 'Gm', 'Am', 'Hm',
+            'C♯', 'D♯', 'E♯', 'F♯', 'G♯', 'A♯', 'H♯',
+            'C♯m', 'D♯m', 'E♯m', 'F♯m', 'G♯m', 'A♯m', 'H♯m',
+            'C♭', 'D♭', 'E♭', 'F♭', 'G♭', 'A♭', 'H♭',
+            'C♭m', 'D♭m', 'E♭m', 'F♭m', 'G♭m', 'A♭m', 'H♭m'
+        ];
+
     return [
         'band_id' => 1,
         'title' => rtrim($faker->realText(20, 3), ".,:;'-"),
         'artist' => 'The ' . $faker->jobTitle . 's',
         'music_by' => $faker->name,
-        'lyrics_by' => $faker->name
+        'lyrics_by' => $faker->name,
+        'key' => $faker->randomElement($musicalKeys),
+        'bpm' => $faker->numberBetween(70, 130),
+        'duration' => $faker->numberBetween(180, 240),
+        'intensity' => $faker->numberBetween(1, 10),
     ];
 });
 
