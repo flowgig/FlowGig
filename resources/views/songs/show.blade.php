0db0ea7d15b4f146c09577ad6f1dbd2f28808dbe
Jørgen Tellnes <jorgen@tellnes.nu>
2017-03-14 15:47:01 +0100

    Display song data and edit-button

--- resources/views/songs/show.blade.php
+++ resources/views/songs/show.blade.php
@@ -32,6 +32,38 @@
                     </li>
                 </ol>
                 <h1>{{ $song->title }}</h1>
+                <div class="text-right">
+                    <a class="button button-icon button-flat button-default tooltip" title="Edit {{$song->title}}"
+                       href="{{ route('songs.edit', $song) }}"><span class="fa fa-pencil"></span>
+                    </a>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <ul class="list">
+                            <li><b>Artist: </b> {{ $song->artist }}</li>
+                            <li><b>Lyrics: </b> {{ $song->lyrics_by }}</li>
+                            <li><b>Music: </b> {{ $song->music_by }}</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <p><i>The following properties are defaults which may be set
+                                different for each instance of this song in setlists</i>
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <ul class="list">
+                            <li><b>Key: </b> {{ $song->key }}</li>
+                            <li><b>BPM: </b> {{ $song->bpm }}</li>
+                            <li><b>Duration: </b> {{ $song->duration }}</li>
+                            <li><b>Intensity: </b> {{ $song->intensity }}</li>
+                        </ul>
+                    </div>
+                </div>
+                @include('meta.user-timestamps', ['model' => $song])
                 <div class="block text-right">
                     <a class="button button-flat button-default" href="{{ route('songs.index', $song->band) }}">Back to song list</a>
                 </div>
