893a789e96c5a35dad892ff039c439b2024f9e54
Jørgen Tellnes <jorgen@tellnes.nu>
2017-04-07 22:17:14 +0200

    Provide export setlist function in show view

--- resources/views/setlists/show.blade.php
+++ resources/views/setlists/show.blade.php
@@ -1,6 +1,61 @@
 @extends('layouts.master', ['currentBand' => $setlist->gig->band])
 @section('title', 'Setlist - ' . $setlist->gig->name)
 @section('navbar-title', $setlist->gig->name)
+@section('actionbar')
+    <div class="action-button">
+        <i class="icon fa fa-plus"></i>
+    </div>
+    <div class="action-menu" id="action-menu">
+        <button class="toggle-action-button toggle-modal tooltip" data-type="radio" title="Export setlist"
+                value="export-setlist-modal">
+                <span class="icon fa fa-download">
+                </span>
+        </button>
+    </div>
+    <div class="modal export-setlist-modal">
+        <div class="modal-container">
+            <div class="modal-header">
+                Export setlist:
+                <button class="modal-close toggle-modal"
+                        value="export-setlist-modal"></button>
+            </div>
+            <div class="modal-content">
+                <p>Toggle fields in setlist</p>
+                <form action="{{ route('setlists.export', $setlist) }}" method="POST" target="_blank">
+                    {{ csrf_field() }}
+                    <div class="input-group">
+                        <input type="checkbox" name="number_in_list" id="number_in_list"/>
+                        <label for="number_in_list">Number in list</label>
+                    </div>
+                    <div class="input-group">
+                        <input type="checkbox" name="key" id="key"/>
+                        <label for="key">Key</label>
+                    </div>
+                    <div class="input-group">
+                        <input type="checkbox" name="bpm" id="bpm"/>
+                        <label for="bpm">BPM</label>
+                    </div>
+                    <input type="checkbox" name="duration" id="duration"/>
+                    <label for="duration">Duration</label>
+                    <div class="input-group">
+                        <input type="checkbox" name="intensity" id="intensity"/>
+                        <label for="intensity">Intensity</label>
+                    </div>
+                    <div class="input-group">
+                        <input type="checkbox" name="comment" id="comment"/>
+                        <label for="comment">Comment</label>
+                    </div>
+                    <div class="input-group float-right">
+                        <button type="submit" class="button button-flat button-default tooltip"
+                                title="Export {{ $setlist->gig->name }} to PDF">
+                            <label for="export">Export to PDF</label>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
 @section('content')
     <div class="content">
         <div class="box">
