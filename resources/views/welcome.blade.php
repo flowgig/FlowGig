4d745a70b88147fa140a013350172504d38e7a3b
Jørgen Tellnes <jorgen@tellnes.nu>
2017-05-10 20:21:11 +0200

    Enable register button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="input-group float-right">
-        {{--<a href="{{ route('register') }}" class="button button-raised">Register</a>--}}
+        <a href="{{ route('register') }}" class="button button-raised">Register</a>
         <a href="{{ route('login') }}" class="button button-primary">Log in</a>
         <div class="clearfix"></div>
     </div>
