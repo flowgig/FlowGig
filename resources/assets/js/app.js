e08eea683b4c61fbb766c78f62b2ca3725ced01e
benjamindehli <superelg@gmail.com>
2017-05-15 00:42:38 +0200

    Initialize axios in main script file

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,6 +7,21 @@
 // require('./bootstrap');
 import Vue from 'vue';
 
+
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common = {
+    'X-CSRF-TOKEN': window.Laravel.csrfToken,
+    'X-Requested-With': 'XMLHttpRequest'
+};
+
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
