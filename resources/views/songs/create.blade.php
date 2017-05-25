6ffc7ef41629904c819b5f69d9d257018bac24ed
benjamindehli <superelg@gmail.com>
2017-05-25 16:23:45 +0200

    Remove unused code

--- resources/views/songs/create.blade.php
+++ resources/views/songs/create.blade.php
@@ -3,29 +3,25 @@
 @section('navbar-title', 'Create song')
 @section('content')
     <div class="content">
-        <div class="box">
-            <div class="content">
-                <breadcrumbs
-                        v-bind:breadcrumb-items='[
+        <breadcrumbs
+                v-bind:breadcrumb-items='[
                         {name: "Bands", link: "{{ route('bands.index') }}"},
                         {name: "{{ $band->name }}", link: "{{ route('bands.show', $band) }}"},
                         {name: "Songs", link: "{{ route('songs.index', $band) }}"},
                         {name: "Create new song", link: "{{ route('songs.create', $band) }}"}
                         ]'>
-                </breadcrumbs>
-                <h1>Create new song</h1>
-                <edit-song v-bind:form-data="{postUrl: '{{ route('songs.store', $band) }}', newInstance: true}"></edit-song>
-                @include('errors.validation-errors')
-                <div class="page-footer">
-                    <div class="button-row">
-                        <custom-button-row
-                                v-bind:button-row="{
+        </breadcrumbs>
+        <h1>Create new song</h1>
+        <edit-song v-bind:form-data="{postUrl: '{{ route('songs.store', $band) }}', newInstance: true}"></edit-song>
+        @include('errors.validation-errors')
+        <div class="page-footer">
+            <div class="button-row">
+                <custom-button-row
+                        v-bind:button-row="{
                                 buttons: [
                                     {link: '{{ route('songs.index', $band) }}', type: 'raised', content: 'Back to song list'}                                ]
                             }">
-                        </custom-button-row>
-                    </div>
-                </div>
+                </custom-button-row>
             </div>
         </div>
     </div>
