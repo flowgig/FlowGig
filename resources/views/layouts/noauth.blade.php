4a7ffa6e24617e15685b4f675e951cbfae407d6d
Jørgen Tellnes <jorgen@tellnes.nu>
2017-06-08 00:05:17 +0200

    Use Piwik for site analytics

--- resources/views/layouts/noauth.blade.php
+++ resources/views/layouts/noauth.blade.php
@@ -3,25 +3,23 @@
 <head>
     <title>FlowGig - @yield('title')</title>
     @include('layouts.header')
+    <!-- Piwik -->
+    <script type="text/javascript">
+        var _paq = _paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="//analytics.flowgig.com/";
+            _paq.push(['setTrackerUrl', u+'piwik.php']);
+            _paq.push(['setSiteId', '1']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+        })();
+    </script>
+    <!-- End Piwik Code -->
 </head>
 <body class="welcome-screen">
-<script>
-    (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function () {
-                    (i[r].q = i[r].q || []).push(arguments)
-                }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-    ga('create', 'UA-79197814-1', 'auto');
-    ga('send', 'pageview');
-
-</script>
 <div class="container">
     <div class="content">
         <div class="title">
