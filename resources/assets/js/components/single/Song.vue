032352be79c52c9abae93d494a59d45f79a772b3
benjamindehli <superelg@gmail.com>
2017-09-03 21:28:28 +0200

    Add action button to song component

--- resources/assets/js/components/single/Song.vue
+++ resources/assets/js/components/single/Song.vue
@@ -1,26 +1,18 @@
 <script>
     import * as quark from 'quark-gui';
     import SongTemplate from './SongTemplate.quark';
+
     export default {
         name: 'Song',
         props: ['formData'],
         template: SongTemplate,
-        data () {
+        data() {
             return {
                 form: {
                     data: this.formData,
                     readOnly: this.formData.viewType == 'show'
                 },
                 csrfToken: window.Laravel.csrfToken
-            }
-        },
-        methods: {
-            getActionButtonText: function () {
-                let actionButtonText = '';
-                if (this.formData.viewType == 'create') actionButtonText = 'Create';
-                else if (this.formData.viewType == 'edit') actionButtonText = 'Update';
-                else if (this.formData.viewType == 'show') actionButtonText = 'Edit';
-                return actionButtonText;
             }
         }
     }
