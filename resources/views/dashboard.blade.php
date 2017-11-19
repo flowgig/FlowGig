575e2f6b6ea26fff41a9009c53d6ede2cf42700d
Benjamin Dehli <benjamin@arkitektum.no>
2017-11-19 16:58:22 +0100

    Remove unused code

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,43 +6,42 @@
         <div class="box">
             <div class="content">
                 <div class="hidden">
-                <breadcrumbs
-                        v-bind:breadcrumb-items='[
+                    <breadcrumbs
+                            v-bind:breadcrumb-items='[
                             {name: "FlowGig", link: "/"}
                         ]'>
-                </breadcrumbs>
+                    </breadcrumbs>
                 </div>
                 <h1>My FlowGig</h1>
 
-                <div id="dashboard" class="content">
-                    <div class="content-container raised">
-                        <div class="content-container-header"><h2>Bands</h2></div>
-                        @if($user->bands->count() > 0)
+                <div class="content-container raised">
+                    <div class="content-container-header"><h2>Bands</h2></div>
+                    @if($user->bands->count() > 0)
                         <bands v-bind:list-items="{{ $user->bands }}"></bands>
-                        @else
-                            <p>You're not a member of any bands yet</p>
-                        @endif
-                        <div class="content-container-footer list-footer-button-row">
-                            <custom-button-row
-                                    v-bind:button-row="{
+                    @else
+                        <p>You're not a member of any bands yet</p>
+                    @endif
+                    <div class="content-container-footer list-footer-button-row">
+                        <custom-button-row
+                                v-bind:button-row="{
                                         buttons: [
                                             {link: '{{ route('bands.index') }}', content: 'Manage bands'},
                                             {link: '{{ route('bands.create') }}', theme: 'primary', content: 'New band'},
                                         ]
                                     }">
-                            </custom-button-row>
-                        </div>
+                        </custom-button-row>
                     </div>
-                    <div class="spacer-vertical-36"></div>
-                    <div class="content-container raised">
-                        <div class="content-container-header"><h2>Upcoming gigs</h2></div>
-                        @if($user->upcomingGigs()->count() > 0)
+                </div>
+                <div class="spacer-vertical-36"></div>
+                <div class="content-container raised">
+                    <div class="content-container-header"><h2>Upcoming gigs</h2></div>
+                    @if($user->upcomingGigs()->count() > 0)
                         <gigs v-bind:list-items="{{ $user->upcomingGigs()->sortBy('date') }}"></gigs>
-                        @else
+                    @else
                         <p>You have no upcoming gigs at the moment</p>
-                        @endif
-                        <div class="content-container-footer list-footer-button-row">
-                           <!-- <custom-button-row // TODO add buttons for gigs
+                    @endif
+                    <div class="content-container-footer list-footer-button-row">
+                    <!-- <custom-button-row // TODO add buttons for gigs
                                     v-bind:button-row="{
                                         buttons: [
                                             {link: '{{ route('bands.index') }}', content: 'Manage bands'},
@@ -50,7 +49,6 @@
                                         ]
                                     }">
                             </custom-button-row>-->
-                        </div>
                     </div>
                 </div>
             </div>
