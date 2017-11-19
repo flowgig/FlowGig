9142984bf8c5278c4f573f89aa6c3506edaa043f
Benjamin Dehli <benjamin@arkitektum.no>
2017-11-19 16:32:30 +0100

    Add Vue components and quark templates for bands list

---
+++ resources/assets/js/components/list-items/Band.vue
@@ -0,0 +1,23 @@
+<script>
+    import * as quark from 'quark-gui';
+    import BandTemplate from './BandTemplate.quark';
+
+    export default {
+        name: 'Band',
+        props: ['listItem'],
+        template: BandTemplate,
+        computed: {
+            links: function () {
+                return {
+                    show: '/bands/' + this.listItem.id,
+                    edit: '/bands/' + this.listItem.id + '/edit'
+                }
+            }
+        },
+        data() {
+            return {
+                csrfToken: window.Laravel.csrfToken
+            }
+        }
+    }
+</script>
---
+++ resources/assets/js/components/lists/Bands.vue
@@ -0,0 +1,14 @@
+<script>
+    import * as quark from 'quark-gui';
+    import Band from "../list-items/Band";
+    import BandsTemplate from './BandsTemplate.quark';
+
+    export default {
+        name: 'Bands',
+        props: ['listItems'],
+        template: BandsTemplate,
+        components: {
+            Band: Band
+        }
+    }
+</script>
