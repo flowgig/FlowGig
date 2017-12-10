1d81ba9a152d5274d9caaa66292a7ba50e1e6333
Benjamin Dehli <benjamin@arkitektum.no>
2017-12-10 01:58:14 +0100

    Add component for repertoire

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -41,10 +41,11 @@ Vue.component('BandMembers', require('./components/lists/BandMembers.vue'));
 Vue.component('BandNavigation', require('./components/lists/BandNavigation.vue'));
 Vue.component('Bands', require('./components/lists/Bands.vue'));
 Vue.component('Gigs', require('./components/lists/Gigs.vue'));
+Vue.component('Invitations', require('./components/lists/Invitations.vue'));
+Vue.component('Repertoire', require('./components/lists/Repertoire.vue'));
 Vue.component('Setlist', require('./components/lists/Setlist.vue'));
 Vue.component('Setlists', require('./components/lists/Setlists.vue'));
 Vue.component('Songs', require('./components/lists/Songs.vue'));
-Vue.component('Invitations', require('./components/lists/Invitations.vue'));
 
 
 /* Grid items */
---
+++ resources/assets/js/components/lists/Repertoire.vue
@@ -0,0 +1,14 @@
+<script>
+    import * as quark from 'quark-gui';
+    import RepertoireSong from "../list-items/RepertoireSong";
+    import RepertoireTemplate from './RepertoireTemplate.quark';
+
+    export default {
+        name: 'Repertoire',
+        props: ['listItems'],
+        template: RepertoireTemplate,
+        components: {
+            RepertoireSong: RepertoireSong
+        }
+    }
+</script>
