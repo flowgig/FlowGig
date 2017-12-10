97023155eb584eaea678fbddba88106ecf5c7550
Benjamin Dehli <benjamin@arkitektum.no>
2017-12-10 01:57:30 +0100

    Add component for repertoire song

---
+++ resources/assets/js/components/list-items/RepertoireSong.vue
@@ -0,0 +1,27 @@
+<script>
+    import * as quark from 'quark-gui';
+    import RepertoireSongTemplate from './RepertoireSongTemplate.quark';
+
+    export default {
+        name: 'RepertoireSong',
+        props: ['listItem'],
+        template: RepertoireSongTemplate,
+        computed: {
+            subTitle: function () {
+                let subTitle = this.listItem.artist;
+                return subTitle;
+            },
+            links: function () {
+                return {
+                    show: '/songs/' + this.listItem.id,
+                    edit: '/songs/' + this.listItem.id + '/edit'
+                }
+            }
+        },
+        data() {
+            return {
+                csrfToken: window.Laravel.csrfToken
+            }
+        }
+    }
+</script>
